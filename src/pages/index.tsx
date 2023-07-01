b581b344b67e2b065c7b0c0dd7370947098d9e37
theisolatedmonk <96rahulseth@gmail.com>
2023-07-01 23:09:39 +0530

    button value entered

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,22 +9,114 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const initialValues = [
-    { value: "" },
-    { value: "" },
-    { value: "" },
-    { value: "" },
-    { value: "" },
-    { value: "" },
-    { value: "" },
-    { value: "" },
-    { value: "" },
+    { value: "" , disabled: false },
+    { value: "" , disabled: false },
+    { value: "" , disabled: false },
+    { value: "" , disabled: false },
+    { value: "" , disabled: false },
+    { value: "" , disabled: false },
+    { value: "" , disabled: false },
+    { value: "" , disabled: false },
+    { value: "" , disabled: false },
   ];
-  const [containerState, setContainerState] = useState(initialValues);
+  const [values, setValues] = useState(initialValues );
   const [currentPlayer, setCurrentPlayer] = useState("x");
+  const [currentValue, setCurrentValue] = useState("X");
+  
 
-  function handleBoxClick() {
+//   const winner = 
+// // {0,1,2
+// // 3,4,5
+// // 6,7,8
+// // 0,4,8
+// // 2,4,6}
+
+
+//    if([index].value ==[]= 'X') 
+
+
+const checkWinCondition = () => {
+  if (
+    values[0].value === "X" &&
+    values[1].value === "X" &&
+    values[2].value === "X"
+  ) {
+    return "X wins";
+  }
+  
+  if (
+    values[3].value === "X" &&
+    values[4].value === "X" &&
+    values[5].value === "X"
+  ) {
+    return "X wins";
+  }
+  if (
+    values[6].value === "X" &&
+    values[7].value === "X" &&
+    values[8].value === "X"
+  ) {
+    return "X wins";
+  }
+  if (
+    values[2].value === "X" &&
+    values[4].value === "X" &&
+    values[6].value === "X"
+  ) {
+    return "X wins";
+  }
+   if (
+    values[0].value === "O" &&
+    values[1].value === "O" &&
+    values[2].value === "O"
+  ) {
+    return "O wins";
+  }
+  if (
+    values[3].value === "O" &&
+    values[4].value === "O" &&
+    values[5].value === "O"
+  ) {
+    return "O wins";
+  }
+  if (
+    values[6].value === "O" &&
+    values[7].value === "O" &&
+    values[8].value === "O"
+  ) {
+    return "O wins";
+  }
+  if (
+    values[2].value === "O" &&
+    values[4].value === "O" &&
+    values[6].value === "O"
+  ) {
+    return "X wins";
+  }
+};
+
+
+  const handleClick = (index:  number) => {
+
     if (currentPlayer === "x") setCurrentPlayer("o");
     else if (currentPlayer === "o") setCurrentPlayer("x");
+    
+
+    // Create a copy of the values array
+    const updatedValues = [...values];
+    // Assign a new value at the specified index
+    updatedValues[index].value =String (currentValue);
+    updatedValues[index].disabled = true;
+    // Update the state with the modified values
+    setValues(updatedValues);
+    setCurrentValue(currentValue === "X" ? "O" : "X");
+  
+  };
+
+
+  const handleReset =() => {
+    setValues(initialValues);
+    setCurrentPlayer("x")
   }
 
   return (
@@ -36,12 +128,12 @@ export default function Home() {
         Tik-Tak-To
       </p>
       <div className="grid grid-cols-3 justify-center w-96 bg-blue-100 h-96 items-center gap-2 p-4 rounded-xl shadow-2xl">
-        {initialValues.map((link) => (
+      {values.map((item, index) => (
           <button
-            onClick={handleBoxClick}
+          key={index} onClick={() => handleClick(index)} disabled={item.disabled}
             className="h-28 w-full rounded-lg bg-white focus:bg-slate-400 focus:outline shadow-lg"
           >
-            {" "}
+           {item.value}
           </button>
         ))}
       </div>
@@ -64,12 +156,13 @@ export default function Home() {
         </p>
       </div>
 
-      <button
+      <button onClick={() => handleReset()}
         className="w-52 h-12
 bg-lime-950 text-white font-bold font-serif rounded-lg   focus:outline focus:outline-white focus:order-2 focus:bg-gray-400 "
       >
         Reset
       </button>
+      <div className="bg-green-500">{checkWinCondition()}</div>
     </main>
   );
 }
