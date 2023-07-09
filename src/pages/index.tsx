7d309cdfee939a2b7f757d93299803d647d363b5
theisolatedmonk <96rahulseth@gmail.com>
2023-07-09 17:26:46 +0530

    winning condition implimented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ImCross } from "react-icons/im";
 import { CgShapeCircle } from "react-icons/cg";
 import clsx from "clsx";
@@ -9,115 +9,82 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const initialValues = [
-    { value: "" , disabled: false },
-    { value: "" , disabled: false },
-    { value: "" , disabled: false },
-    { value: "" , disabled: false },
-    { value: "" , disabled: false },
-    { value: "" , disabled: false },
-    { value: "" , disabled: false },
-    { value: "" , disabled: false },
-    { value: "" , disabled: false },
+    { value: "", disabled: false },
+    { value: "", disabled: false },
+    { value: "", disabled: false },
+    { value: "", disabled: false },
+    { value: "", disabled: false },
+    { value: "", disabled: false },
+    { value: "", disabled: false },
+    { value: "", disabled: false },
+    { value: "", disabled: false },
   ];
-  const [values, setValues] = useState(initialValues );
+  const [values, setValues] = useState(initialValues);
   const [currentPlayer, setCurrentPlayer] = useState("x");
   const [currentValue, setCurrentValue] = useState("X");
-  
+  const [winPlayer, setWinPlayer] = useState<string | null>(null);
 
-//   const winner = 
-// // {0,1,2
-// // 3,4,5
-// // 6,7,8
-// // 0,4,8
-// // 2,4,6}
+  useEffect(() => {
+    if (
+      (values[0].value === "X" &&
+        values[1].value === "X" &&
+        values[2].value === "X") ||
+      (values[3].value === "X" &&
+        values[4].value === "X" &&
+        values[5].value === "X") ||
+      (values[6].value === "X" &&
+        values[7].value === "X" &&
+        values[8].value === "X") ||
+      (values[2].value === "X" &&
+        values[4].value === "X" &&
+        values[6].value === "X") ||
+      (values[0].value === "X" &&
+        values[4].value === "X" &&
+        values[8].value === "X")
+    )
+      setWinPlayer("X");
+    if (
+      (values[0].value === "O" &&
+        values[1].value === "O" &&
+        values[2].value === "O") ||
+      (values[3].value === "O" &&
+        values[4].value === "O" &&
+        values[5].value === "O") ||
+      (values[6].value === "O" &&
+        values[7].value === "O" &&
+        values[8].value === "O") ||
+      (values[2].value === "O" &&
+        values[4].value === "O" &&
+        values[6].value === "O") ||
+      (values[0].value === "O" &&
+        values[4].value === "O" &&
+        values[8].value === "O")
+    )
+      setWinPlayer("O");
 
+    // return () => {
+    //   second
+    // }
+  }, [values]);
 
-//    if([index].value ==[]= 'X') 
-
-
-const checkWinCondition = () => {
-  if (
-    values[0].value === "X" &&
-    values[1].value === "X" &&
-    values[2].value === "X"
-  ) {
-    return "X wins";
-  }
-  
-  if (
-    values[3].value === "X" &&
-    values[4].value === "X" &&
-    values[5].value === "X"
-  ) {
-    return "X wins";
-  }
-  if (
-    values[6].value === "X" &&
-    values[7].value === "X" &&
-    values[8].value === "X"
-  ) {
-    return "X wins";
-  }
-  if (
-    values[2].value === "X" &&
-    values[4].value === "X" &&
-    values[6].value === "X"
-  ) {
-    return "X wins";
-  }
-   if (
-    values[0].value === "O" &&
-    values[1].value === "O" &&
-    values[2].value === "O"
-  ) {
-    return "O wins";
-  }
-  if (
-    values[3].value === "O" &&
-    values[4].value === "O" &&
-    values[5].value === "O"
-  ) {
-    return "O wins";
-  }
-  if (
-    values[6].value === "O" &&
-    values[7].value === "O" &&
-    values[8].value === "O"
-  ) {
-    return "O wins";
-  }
-  if (
-    values[2].value === "O" &&
-    values[4].value === "O" &&
-    values[6].value === "O"
-  ) {
-    return "X wins";
-  }
-};
-
-
-  const handleClick = (index:  number) => {
-
+  const handleClick = (index: number) => {
     if (currentPlayer === "x") setCurrentPlayer("o");
     else if (currentPlayer === "o") setCurrentPlayer("x");
-    
 
     // Create a copy of the values array
     const updatedValues = [...values];
     // Assign a new value at the specified index
-    updatedValues[index].value =String (currentValue);
+    updatedValues[index].value = String(currentValue);
     updatedValues[index].disabled = true;
     // Update the state with the modified values
     setValues(updatedValues);
     setCurrentValue(currentValue === "X" ? "O" : "X");
-  
   };
 
-
-  const handleReset =() => {
+  const handleReset = () => {
     setValues(initialValues);
-    setCurrentPlayer("x")
-  }
+    setCurrentPlayer("x");
+  };
 
   return (
     <main className=" flex flex-col justify-center bg-white h-screen w-full items-center gap-2">
@@ -128,12 +95,14 @@ const checkWinCondition = () => {
         Tik-Tak-To
       </p>
       <div className="grid grid-cols-3 justify-center w-96 bg-blue-100 h-96 items-center gap-2 p-4 rounded-xl shadow-2xl">
-      {values.map((item, index) => (
+        {values.map((item, index) => (
           <button
-          key={index} onClick={() => handleClick(index)} disabled={item.disabled}
+            key={index}
+            onClick={() => handleClick(index)}
+            disabled={item.disabled}
             className="h-28 w-full rounded-lg bg-white focus:bg-slate-400 focus:outline shadow-lg"
           >
-           {item.value}
+            {item.value}
           </button>
         ))}
       </div>
@@ -156,13 +125,24 @@ const checkWinCondition = () => {
         </p>
       </div>
 
-      <button onClick={() => handleReset()}
+      <button
+        onClick={() => handleReset()}
         className="w-52 h-12
 bg-lime-950 text-white font-bold font-serif rounded-lg   focus:outline focus:outline-white focus:order-2 focus:bg-gray-400 "
       >
         Reset
       </button>
-      <div className="bg-green-500">{checkWinCondition()}</div>
+      {/* <div className="bg-green-500">{()}</div> */}
+
+      {/*  */}
+      <>
+        {winPlayer && (
+          <div className="h-screen w-full bg-black/90 fixed flex items-center justify-center text-white  ">
+            <button className="font-bold border px-4 border-black absolute top-20 right-20" onClick={() => setWinPlayer(null)}>Close</button>
+            <p className="text-5xl  font-bold">Player {winPlayer} Win </p>
+          </div>
+        )}
+      </>
     </main>
   );
 }
