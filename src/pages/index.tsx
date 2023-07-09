902a37eda64e4c509266a8b7ee2d091ad87be4a7
theisolatedmonk <96rahulseth@gmail.com>
2023-07-09 19:47:10 +0530

    animation added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,11 @@ import { useEffect, useState } from "react";
 import { ImCross } from "react-icons/im";
 import { CgShapeCircle } from "react-icons/cg";
 import clsx from "clsx";
-
 const inter = Inter({ subsets: ["latin"] });
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 export default function Home() {
+  const [parent, enableAnimations] = useAutoAnimate(/* optional config */);
   const initialValues = [
     { value: "", disabled: false },
     { value: "", disabled: false },
@@ -24,44 +25,49 @@ export default function Home() {
   const [currentValue, setCurrentValue] = useState("X");
   const [winPlayer, setWinPlayer] = useState<string | null>(null);
 
-  useEffect(() => {
-    if (
-      (values[0].value === "X" &&
-        values[1].value === "X" &&
-        values[2].value === "X") ||
-      (values[3].value === "X" &&
-        values[4].value === "X" &&
-        values[5].value === "X") ||
-      (values[6].value === "X" &&
-        values[7].value === "X" &&
-        values[8].value === "X") ||
-      (values[2].value === "X" &&
-        values[4].value === "X" &&
-        values[6].value === "X") ||
-      (values[0].value === "X" &&
-        values[4].value === "X" &&
-        values[8].value === "X")
-    )
-      setWinPlayer("X");
-    if (
-      (values[0].value === "O" &&
-        values[1].value === "O" &&
-        values[2].value === "O") ||
-      (values[3].value === "O" &&
-        values[4].value === "O" &&
-        values[5].value === "O") ||
-      (values[6].value === "O" &&
-        values[7].value === "O" &&
-        values[8].value === "O") ||
-      (values[2].value === "O" &&
-        values[4].value === "O" &&
-        values[6].value === "O") ||
-      (values[0].value === "O" &&
-        values[4].value === "O" &&
-        values[8].value === "O")
-    )
-      setWinPlayer("O");
+  const isXTrue =
+    (values[0].value === "X" &&
+      values[1].value === "X" &&
+      values[2].value === "X") ||
+    (values[3].value === "X" &&
+      values[4].value === "X" &&
+      values[5].value === "X") ||
+    (values[6].value === "X" &&
+      values[7].value === "X" &&
+      values[8].value === "X") ||
+    (values[2].value === "X" &&
+      values[4].value === "X" &&
+      values[6].value === "X") ||
+    (values[0].value === "X" &&
+      values[4].value === "X" &&
+      values[8].value === "X") ||
+    (values[0].value === "X" &&
+      values[3].value === "X" &&
+      values[6].value === "X");
 
+  const isOTrue =
+    (values[0].value === "O" &&
+      values[1].value === "O" &&
+      values[2].value === "O") ||
+    (values[3].value === "O" &&
+      values[4].value === "O" &&
+      values[5].value === "O") ||
+    (values[6].value === "O" &&
+      values[7].value === "O" &&
+      values[8].value === "O") ||
+    (values[2].value === "O" &&
+      values[4].value === "O" &&
+      values[6].value === "O") ||
+    (values[0].value === "O" &&
+      values[4].value === "O" &&
+      values[8].value === "O");
+
+  const isBoxFilled = values.every((value) => value.value != "");
+
+  useEffect(() => {
+    if (isXTrue) setWinPlayer(` Player "X" Win`);
+    if (isOTrue) setWinPlayer(` Player "O" Win`);
+    if (!isXTrue && !isOTrue && isBoxFilled) setWinPlayer(" Match Draw");
     // return () => {
     //   second
     // }
@@ -87,7 +93,10 @@ export default function Home() {
   };
 
   return (
-    <main className=" flex flex-col justify-center bg-white h-screen w-full items-center gap-2">
+    <main
+      ref={parent}
+      className=" flex flex-col justify-center bg-white h-screen w-full items-center gap-2"
+    >
       <p
         className="w-52 h-10
  text-lime-950 text-3xl font-bold font-serif rounded-lg  text-center items-center"
@@ -100,17 +109,21 @@ export default function Home() {
             key={index}
             onClick={() => handleClick(index)}
             disabled={item.disabled}
-            className="h-28 w-full rounded-lg bg-white focus:bg-slate-400 focus:outline shadow-lg"
+            className={clsx(
+              "h-28 w-full rounded-lg text-2xl font-bold bg-white focus:bg-slate-400 focus:outline shadow-lg",
+              { "!text-blue-800": item.value == "X" }
+            )}
           >
             {item.value}
           </button>
         ))}
       </div>
+
       <div className="grid grid-cols-2  rounded-md p-2 w-40 gap-4  gap h-14 items-center ">
         <p
           className={clsx(
             "  h-10 rounded-xl flex items-center justify-center",
-            currentPlayer === "x" ? "bg-gray-400" : "bg-white"
+            currentPlayer == "x" ? "bg-green-400" : "bg-white"
           )}
         >
           <ImCross />
@@ -118,7 +131,7 @@ export default function Home() {
         <p
           className={clsx(
             "  h-10 rounded-xl flex items-center justify-center ",
-            currentPlayer === "o" ? "bg-gray-400" : "bg-white"
+            currentPlayer == "o" ? "bg-green-400" : "bg-white"
           )}
         >
           <CgShapeCircle className="h-8 w-8" />
@@ -135,14 +148,19 @@ bg-lime-950 text-white font-bold font-serif rounded-lg   focus:outline focus:out
       {/* <div className="bg-green-500">{()}</div> */}
 
       {/*  */}
-      <>
-        {winPlayer && (
-          <div className="h-screen w-full bg-black/90 fixed flex items-center justify-center text-white  ">
-            <button className="font-bold border px-4 border-black absolute top-20 right-20" onClick={() => setWinPlayer(null)}>Close</button>
-            <p className="text-5xl  font-bold">Player {winPlayer} Win </p>
-          </div>
-        )}
-      </>
+      {winPlayer && (
+        <div className="h-screen  w-full bg-black/90 fixed flex items-center justify-center text-white  ">
+          <button
+            className="font-bold border px-4 border-black absolute top-20 right-20"
+            onClick={() => {
+              setWinPlayer(null), handleReset(), setCurrentPlayer("x");
+            }}
+          >
+            Close
+          </button>
+          <p className="text-5xl  font-bold"> {winPlayer} </p>
+        </div>
+      )}
     </main>
   );
 }
